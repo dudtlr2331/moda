76aa285e8f98fa571124d3eb806371393c15b811
YS <dudtlr102@naver.com>
2023-10-06 23:46:01 +0900

    초기 셋팅

---
+++ moda/src/main/java/com/moda/moda/ModaApplication.java
@@ -0,0 +1,13 @@
+package com.moda.moda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModaApplication.class, args);
+	}
+
+}
---
+++ moda/src/test/java/com/moda/moda/ModaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moda.moda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
