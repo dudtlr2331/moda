cc496eb41007408d4154497bdfcccf323c4d7084
YS <dudtlr102@naver.com>
2023-10-25 23:31:48 +0900

    join 삭제

--- moda/src/main/java/com/moda/moda/join/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.moda.moda.join.repository;
-
-import com.moda.moda.join.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-}
