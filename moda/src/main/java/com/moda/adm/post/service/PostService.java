422ef0db1a2cf07436886e46f06cf97a71b839d8
YS <dudtlr102@naver.com>
2023-10-30 23:46:57 +0900

    게시글 서비스 수정

--- moda/src/main/java/com/moda/adm/post/service/PostService.java
+++ moda/src/main/java/com/moda/adm/post/service/PostService.java
@@ -1,4 +1,14 @@
 package com.moda.adm.post.service;
 
+import com.moda.adm.post.PostDto;
+import com.moda.adm.post.PostSearch;
+
+import java.util.List;
+
 public interface PostService {
+    public Long savePost(final PostSearch params);
+    public PostDto findPostById(final Long id) ;
+    public Long updatePost(final PostSearch params);
+    public Long deletePost(final Long id);
+    public List<PostDto> findAllPost();
 }
