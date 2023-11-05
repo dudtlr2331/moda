3ab450f92270964f5733d5327ba8356c09cedf11
주승용 <tmddyd262@gmail.com>
2023-11-05 20:47:48 +0900

    문의 관리 해결 but id와 title이 디스플레이 되지 않는 문제 있음

--- moda/src/main/java/com/moda/adm/qna/QnaDto.java
+++ moda/src/main/java/com/moda/adm/qna/QnaDto.java
@@ -8,11 +8,11 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class QnaDto {           // Response
-    private Long qnaNum;       // 질문 일련 번호
-    private String id;          // PK 작성자 아이디
+    private Long qnaNum;       // PK 질문 일련 번호
+    private String id;          // 작성자 아이디
     private String title;       // 제목
     private String qContent;   // 질문
     private String aContent;   // 답변
-    private Boolean stat;       // 답변 상태
+    private int stat;       // 답변 상태
     private LocalDateTime qnaDate;    // 질문한 날짜 및 시간
 }
--- moda/src/main/java/com/moda/adm/qna/QnaSearch.java
+++ moda/src/main/java/com/moda/adm/qna/QnaSearch.java
@@ -6,10 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class QnaSearch {        // Request
-    private Long qna_num;       // 질문 일련 번호
-    private String id;          // PK 작성자 아이디
+    private Long qnaNum;       // PK 질문 일련 번호
+    private String id;          // 작성자 아이디
     private String title;       // 제목
-    private String q_content;   // 질문
-    private String a_content;   // 답변
-    private Boolean stat;       // 답변 상태
+    private String qContent;   // 질문
+    private String aContent;   // 답변
+    private int stat;       // 답변 상태
 }
