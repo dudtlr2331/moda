51869be0045b5e939c063fdaecad97836211f478
주승용 <tmddyd262@gmail.com>
2023-11-06 18:19:10 +0900

    문의 관리 영역 : 텍스트 출력 안되는 현상 해결

--- moda/src/main/java/com/moda/adm/qna/QnaDto.java
+++ moda/src/main/java/com/moda/adm/qna/QnaDto.java
@@ -9,10 +9,10 @@ import java.time.LocalDateTime;
 @Setter
 public class QnaDto {           // Response
     private Long qnaNum;       // PK 질문 일련 번호
-    private String id;          // 작성자 아이디
-    private String title;       // 제목
+    private String uId;          // 작성자 아이디
+    private String qnaTitle;       // 제목
     private String qContent;   // 질문
     private String aContent;   // 답변
-    private int stat;       // 답변 상태
+    private int qnaStat;       // 답변 상태
     private LocalDateTime qnaDate;    // 질문한 날짜 및 시간
 }
--- moda/src/main/java/com/moda/adm/qna/QnaSearch.java
+++ moda/src/main/java/com/moda/adm/qna/QnaSearch.java
@@ -7,9 +7,9 @@ import lombok.Setter;
 @Setter
 public class QnaSearch {        // Request
     private Long qnaNum;       // PK 질문 일련 번호
-    private String id;          // 작성자 아이디
-    private String title;       // 제목
+    private String uId;          // 작성자 아이디
+    private String qnaTitle;       // 제목
     private String qContent;   // 질문
     private String aContent;   // 답변
-    private int stat;       // 답변 상태
+    private int qnaStat;       // 답변 상태
 }
