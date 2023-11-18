e569286e30a02ba50bf6a314e2a170a57b7ba42e
주승용 <tmddyd262@gmail.com>
2023-11-19 06:59:44 +0900

    로그인, 회원가입, 회원 수정 : 로그인, 회원가입 기능 거의 완료(회원 수정 드가자!)

---
+++ moda/src/main/java/com/moda/moda/member/MemberVO.java
@@ -0,0 +1,17 @@
+package com.moda.moda.member;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+    private int uNum;
+    private String uId;
+    private String uPass;
+    private String uName;
+    private String uEmail;
+    private String uPost;
+    private String uAddr;
+    private String uPhone;
+    private String uDate;
+    private String uAdmin;
+}
