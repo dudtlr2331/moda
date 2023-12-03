3c2661dee84fd1bf1e89aa0dbf2eea235e8940cb
dhwldnjs <yewonjiwon2@gmail.com>
2023-12-03 15:59:40 +0900

    tp_post db추가

--- moda/src/main/db/createTable.sql
+++ moda/src/main/db/createTable.sql
@@ -22,6 +22,18 @@ CREATE TABLE IF NOT EXISTS `cart` (
     PRIMARY KEY (`cart_num`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE IF NOT EXISTS tb_post (
+`id`            bigint(20)    NOT NULL AUTO_INCREMENT COMMENT 'PK',
+`title`         varchar(100)  NOT NULL COMMENT '제목',
+`content`       varchar(3000) NOT NULL COMMENT '내용',
+`writer`        varchar(20)   NOT NULL COMMENT '작성자',
+`view_cnt`      int(11)       NOT NULL COMMENT '조회 수',
+`notice_yn`     tinyint(1)    NOT NULL COMMENT '공지글 여부',
+`delete_yn`     tinyint(1)    NOT NULL COMMENT '삭제 여부',
+`created_date`  datetime      NOT NULL DEFAULT current_timestamp() COMMENT '생성일시',
+`modified_date` datetime               DEFAULT NULL COMMENT '최종 수정일시',
+PRIMARY KEY (`id`)
+) COMMENT '게시글';
 
 CREATE TABLE IF NOT EXISTS CATEGORY
 ( `GOODS_CATA_SEQ`   INT(20)      AUTO_INCREMENT PRIMARY KEY
